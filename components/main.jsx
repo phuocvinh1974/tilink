0e946303bcb00cd43340580faa85a32b6c5d5b5e
phuoc vinh <phuocvinh1974@yahoo.com>
2015-10-14 09:06:33 +0700

    ...

---
+++ components/main.jsx
@@ -0,0 +1,9 @@
+var Main = React.createClass({
+	render: function () {
+		return (
+			<div>
+				Main Prog
+			</div>
+		);
+	}
+});
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+      <meta charset="UTF-8">
+      <title>components</title>
+      <script src="js/react.min.js"></script>
+      <script src="js/JSXTransformer.js"></script>
+      <script src="js/jquery-1.8.2.min.js"></script> 
+      <script src="js/react-with-addons.min.js"></script>
+  </head>
+
+  <body>
+  
+      <script type="text/jsx" src="components/app.jsx"></script>
+      <script type="text/jsx" src="components/login.form.jsx"></script>
+     
+      <script type="text/jsx">
+
+      React.render(<App />, document.body);
+
+      //
+       
+      </script>
+
+      <script type="text/javascript">
+        // function msg (s, z) {
+        //   console.log (s + z)
+          
+        // }
+        // var f = msg 
+        // f('hello world',' I am studing');
+      </script>
+
+  </body>
+
+</html>
